11d4ac1e7a821b90523ddeaf023bba95e9510c88
bigstronkcodeman <43645849+bigstronkcodeman@users.noreply.github.com>
2022-02-19 20:44:03 -0600

    Update Converter.hs

--- Converter.hs
+++ Converter.hs
@@ -9,13 +9,15 @@ type Prompt = String
 data NumberBase = Binary | Octal | Decimal | Hexadecimal
     deriving (Show, Read, Eq, Enum)
 
+-- A mapping from number bases to their integer forms
 baseNum :: (Integral a) => NumberBase -> a
 baseNum Binary = 2
 baseNum Octal = 8
 baseNum Decimal = 10
 baseNum Hexadecimal = 16
 
-
+-- Given a string and a number base, check to see if that string is
+-- a valid representation of a number in the given base
 isValidNumber :: String -> NumberBase -> Bool
 isValidNumber "" _ = False
 isValidNumber s nb = foldl' (\acc d -> acc && (d `elem` validDigs nb)) True s
@@ -25,6 +27,8 @@ isValidNumber s nb = foldl' (\acc d -> acc && (d `elem` validDigs nb)) True s
           validDigs Hexadecimal = "0123456789abcdef"
 
 
+-- Given a string, return a Maybe NumberBase depending on whether or not
+-- a the string represents a valid number in any of the supported number bases
 numberBase :: String -> Maybe NumberBase
 numberBase ds
     | "0b" `isPrefixOf` ds && isValidNumber numStr Binary = Just Binary
@@ -34,12 +38,17 @@ numberBase ds
     | otherwise = Nothing
     where numStr = drop 2 ds
 
+
+-- Add proper prefix to a given number string
 addPrefix :: NumberBase -> String -> String
 addPrefix Binary = ("0b" ++)
 addPrefix Octal = ("0o" ++)
 addPrefix Decimal = id
 addPrefix Hexadecimal = ("0x" ++)
 
+
+-- Given a string, return a Maybe value that will hold the numerical representation
+-- of the string in the proper base, if the string is valid
 strToDecimal :: (Integral a) => String -> Maybe a
 strToDecimal s = 
     numberBase s >>= \b ->
@@ -49,6 +58,8 @@ strToDecimal s =
           modify _ = drop 2 s
 
 
+-- Given an integral number and a number base, convert it to the corresponding
+-- string representation
 numToStr :: (Show a, Integral a) => a -> NumberBase -> String
 numToStr x nb =
     let base = baseNum nb
@@ -57,10 +68,11 @@ numToStr x nb =
     in dropWhile (== '0') $ foldl (\acc (_,r) -> r : acc) "" divRemPairs
 
 
+-- Simple helper for printing a string to stdout then flushing
 putStrFlush :: String -> IO ()
 putStrFlush s = putStr s >> hFlush stdout
 
-
+-- Get a valid number (in any of the supported bases) from the user
 getInputNumber :: (Integral a) => Prompt -> IO a
 getInputNumber p =
     putStrFlush p >>
@@ -69,7 +81,7 @@ getInputNumber p =
         Nothing -> getInputNumber "Invalid number, try again: "
         Just x -> return x
         
-
+-- Get a valid supported number base from the user
 getBase :: Prompt -> IO NumberBase
 getBase p = 
     putStrFlush p >>
@@ -85,7 +97,7 @@ getBase p =
           rest (_:xs) = Just xs
           rest _ = Nothing
 
-
+-- Do the stuff
 main = 
     getInputNumber "Enter an integer: " >>= \numIn ->
     getBase "Enter a base to convert to: " >>= \baseOut -> 
